c7021a89133e05eae78ff77275ad5a35d0a0929f
Rodolfo Gonzalez <rodoart@ciencias.unam.mx>
2022-03-22 18:02:52 -0600

    Adding virtual environment.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,21 +8,27 @@ CURRENT_DIR = pathlib.Path(".").resolve()
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
+GENERATE_VIRTUAL_ENVIROMENT = "{{ cookiecutter.generate_virtual_enviroment }}"
+
 
 print(f"Creating .gitignore file...{RESET_ALL}")
 
 file_object = open('.gitignore', 'a')
-file_object.write('\n/figures/')
+file_object.write('\n/pictures/')
 file_object.write('\n/data/')
 file_object.write('\n*.code-workspace')
 
 print(f"Creating virtual enviroment...{RESET_ALL}")
 
-enviro_path = CURRENT_DIR.joinpath("environment.yml")
-os.system(f'conda env create --file "{enviro_path}"')
+enviro_path = CURRENT_DIR.joinpath("python", "environment.yml")
+print(f'"{enviro_path}"')
 
-print(f"Starting virtual envirmoment...{RESET_ALL}")
-os.system('conda activate {{ cookiecutter.project_slug }}')
+
+if GENERATE_VIRTUAL_ENVIROMENT.lower().startswith("y"):
+    os.system(f'conda env create --file "{enviro_path}"')
+
+    print(f"Starting virtual envirmoment...{RESET_ALL}")
+    os.system('conda activate {{ cookiecutter.project_slug }}')
 
 print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
